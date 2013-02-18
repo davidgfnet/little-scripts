d794b338cdc63b30a8280fe3f3d03bce033f9658
rballester <rballri@gmail.com>
2013-02-18 23:30:00 +0100

    Paper renamer

---
+++ paper_renamer/paper_renamer.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#use CAM::PDF;
+#use CAM::PDF::PageText;
+
+if ($#ARGV == -1) {
+	print "\npaper_renamer: interactively (based on best guess) rename academic papers\n";
+	print "\nUsage: paper_renamer.pl <foldername>\n\n";
+	exit;
+}
+
+recursive($ARGV[0]);
+
+sub recursive {
+	my $name = $_[0];
+	if (-d $name) {
+		chdir $name;
+		my @files = <*>;
+		foreach my $file (@files) {
+			recursive($file);
+		}
+		chdir '..';
+	}
+	else {
+		if ($name =~ m/.*\.pdf/) {
+			fix($name);
+		}
+	}
+}
+
+sub fix {
+	my $filename = $_[0];
+	system("pdftotext -bbox -f 1 -l 1 $filename TEMP.html");
+	open FILE, "TEMP.html";
+	my @lines = <FILE>;
+	@lines = splice @lines, 0, 50;
+	close FILE;
+	system("rm TEMP.html");
+	my $maxHeight = 0;
+	foreach my $line (@lines) {
+		chomp $line;
+		if ($line =~ m/.*<word xMin="\d+\.\d+" yMin="(\d+\.\d+)" xMax="\d+\.\d+" yMax="(\d+\.\d+)">([^<]*)<\/word>.*/) {
+			my $difference = $2 - $1;
+			if ($difference > $maxHeight) {
+				$maxHeight = $difference;
+			}
+		}
+	}
+	my $result = "";
+	foreach my $line (@lines) {
+		chomp $line;
+		if ($line =~ m/.*<word xMin="\d+\.\d+" yMin="(\d+\.\d+)" xMax="\d+\.\d+" yMax="(\d+\.\d+)">([^<]*)<\/word>.*/) {
+			my $difference = $2 - $1;
+			if (abs($maxHeight - $difference) < 0.1) {
+				if ($result eq "") {
+					$result .= $3;
+				}
+				else {
+					$result .= " $3";
+				}
+			}
+		}
+	}
+	$result = substr $result, 0, 127;
+	$result =~ s/ﬁ/fi/g;
+	$result =~ s/ /_/g;
+	$result .= '.pdf';
+	if ($result ne $filename) {
+		print "\n*******************************************\n\n";
+		print "Rename\n";
+		print "\n\"$filename\"\n";
+		print "\nas\n";
+		print "\n\"$result\"? (y/n) ";
+		my $answer = <STDIN>;
+		if ($answer eq "y\n") {
+			system("mv $filename $result");
+			print "\nDone!\n";
+		}
+	}
+}
+
