b57355de079befb3e8640e109d390546d8ef3637
David Guillen Fandos <david@localhost.localdomain>
2013-05-13 22:55:24 +0200

    Small improvement in binary size (good for DLLs).

--- mingw/mingw32.sh
+++ mingw/mingw32.sh
@@ -9,13 +9,13 @@
 # to the executable
 
 VERSION="2.0.7"
-wget "http://switch.dl.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v$VERSION.tar.gz"
+#wget "http://switch.dl.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v$VERSION.tar.gz"
 tar xf mingw-w64-v$VERSION.tar.gz
 cd mingw-w64-v$VERSION
 
 patch mingw-w64-crt/crt/pseudo-reloc.c << EOF
 --- pseudo-reloc.c	2011-12-20 22:44:38.000000000 +0100
-+++ pseudo-reloc.c_	2012-07-23 19:41:54.000000000 +0200
++++ pseudo-reloc.c	2013-05-12 21:21:45.892882497 +0200
 @@ -78,82 +78,8 @@
    DWORD version;
  } runtime_pseudo_reloc_v2;
@@ -44,8 +44,7 @@ patch mingw-w64-crt/crt/pseudo-reloc.c << EOF
 -  va_list args;
 -  HANDLE errh = GetStdHandle (STD_ERROR_HANDLE);
 -  ssize_t modulelen = GetModuleFileNameW (NULL, module, sizeof (module));
-+#define __report_error(...) abort();
- 
+-
 -  if (errh == INVALID_HANDLE_VALUE)
 -    cygwin_internal (CW_EXIT_PROCESS,
 -                     STATUS_ILLEGAL_DLL_PSEUDO_RELOCATION,
@@ -78,7 +77,8 @@ patch mingw-w64-crt/crt/pseudo-reloc.c << EOF
 -      WriteFile (errh, (PCVOID)buf, len, &done, NULL);
 -    }
 -  WriteFile (errh, (PCVOID)"\n", 1, &done, NULL);
--
++#define __report_error(...) abort();
+ 
 -  cygwin_internal (CW_EXIT_PROCESS,
 -                   STATUS_ILLEGAL_DLL_PSEUDO_RELOCATION,
 -                   1);
@@ -97,6 +97,78 @@ patch mingw-w64-crt/crt/pseudo-reloc.c << EOF
 -  abort ();
 -#endif
 -}
+ 
+ /* For mingw-w64 we have additional helpers to get image information
+    on runtime.  This allows us to cache for pseudo-relocation pass
+@@ -190,7 +116,7 @@
+   h = __mingw_GetSectionForAddress (addr);
+   if (!h)
+     {
+-      __report_error ("Address %p has no image-section", addr);
++      __report_error ();
+       return;
+     }
+   the_secs[i].hash = h;
+@@ -199,8 +125,7 @@
+ 
+   if (!VirtualQuery (the_secs[i].sec_start, &b, sizeof(b)))
+     {
+-      __report_error ("  VirtualQuery failed for %d bytes at address %p",
+-		      (int) h->Misc.VirtualSize, the_secs[i].sec_start);
++      __report_error ();
+       return;
+     }
+ 
+@@ -209,8 +134,7 @@
+       if (!VirtualProtect (b.BaseAddress, b.RegionSize,
+ 			   PAGE_EXECUTE_READWRITE,
+ 			   &the_secs[i].old_protect))
+-	__report_error ("  VirtualProtect failed with code 0x%x",
+-	  (int) GetLastError ());
++	__report_error ();
+     }
+   ++maxSections;
+   return;
+@@ -229,9 +153,7 @@
+         continue;
+       if (!VirtualQuery (the_secs[i].sec_start, &b, sizeof(b)))
+ 	{
+-	  __report_error ("  VirtualQuery failed for %d bytes at address %p",
+-			  (int) the_secs[i].hash->Misc.VirtualSize,
+-			  the_secs[i].sec_start);
++	  __report_error ();
+ 	  return;
+ 	}
+       VirtualProtect (b.BaseAddress, b.RegionSize, the_secs[i].old_protect,
+@@ -270,8 +192,7 @@
+ 
+   if (!VirtualQuery (addr, &b, sizeof(b)))
+     {
+-      __report_error ("  VirtualQuery failed for %d bytes at address %p",
+-		      (int) sizeof(b), addr);
++      __report_error ();
+     }
+ 
+   /* Temporarily allow write access to read-only protected memory.  */
+@@ -364,8 +285,7 @@
+   /* Check if this is a known version.  */
+   if (v2_hdr->version != RP_VERSION_V2)
+     {
+-      __report_error ("  Unknown pseudo relocation protocol version %d.\n",
+-		      (int) v2_hdr->version);
++      __report_error ();
+       return;
+     }
+ 
+@@ -419,8 +339,7 @@
+ #endif
+ 	  default:
+ 	    reldata=0;
+-	    __report_error ("  Unknown pseudo relocation bit size %d.\n",
+-		    (int) (r->flags & 0xff));
++	    __report_error ();
+ 	    break;
+         }
 EOF
 
 ./configure --host=i686-w64-mingw32 | tee ../log.txt
